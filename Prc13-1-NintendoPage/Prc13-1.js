ead2cb4487c0b3524e67f95fcb7e26e88b119b38
GreyGao <rh87116938@163.com>
2017-08-09 14:20:26 +0800

    20170809-2

--- Prc13-1-NintendoPage/Prc13-1.js
+++ Prc13-1-NintendoPage/Prc13-1.js
@@ -1,34 +1,44 @@
 let n=1;
 let list = document.querySelector('#gameList');
+let loading = false;
+let hasNext = true;
 loadMore.onclick = function () {
+    if(loading){return}
+    if(!hasNext){return}
     let request = new XMLHttpRequest();
-    request.open('GET',`./poster-${n+1}.json`);
+    request.open('GET', `./poster-${n + 1}.json`);
+    request.onerror = function () {
+        loading = false;
+    };
     request.onload = function () {
         let response = request.responseText;
         let data = JSON.parse(response);
         let li = `
-            <div class="poster poster${n+1}">
-			<a id="pcpos${n+1}" href=""><img src="${data.content.url1}"></a>
-			<a id="mobpos${n+1}" href=""><img src="${data.content.url2}"></a>
-			<div class="nsglink nsglink${n+1}">
+            <div class="poster poster${n + 1}">
+			<a id="pcpos${n + 1}" href=""><img src="${data.content.url1}"></a>
+			<a id="mobpos${n + 1}" href=""><img src="${data.content.url2}"></a>
+			<div class="nsglink nsglink${n + 1}">
 				<a href="">
 					<h2>${data.content.title}</h2></a>
 				<hr class="adwhite">
 				<p>${data.content.text}</p>
-				<a href=""><span class="buybutton buybutton${n+2}">Buy now></span></a>
+				<a href=""><span class="buybutton buybutton${n + 2}">Buy now></span></a>
 			</div>
 		</div>
                 `;
         list.insertAdjacentHTML('beforeend', li);
-        if(data.hasNextPage === false){
+        if (data.hasNextPage === false) {
             loadMore.textContent = "to the end...";
             loadMore.onclick = null;
             loadMore.style.fontSize = "16px";
+            hasNext = false;
         }
-        n+=1;
+        n += 1;
+        loading = false;
     };
+    loading = true;
     request.send()
-    };
+};
 
 window.onscroll = function () {
     var doc =document.documentElement;
--- Prc13-1-NintendoPage/readme.md
+++ Prc13-1-NintendoPage/readme.md
@@ -2,7 +2,10 @@
 
 ## 预览链接
 
+https://greygao.github.io/hello-world/Prc13-1-NintendoPage/index.html
+
 ## 实现功能
 1. 响应式设计
 2. Ajax 页面元素自动加载
+
 
